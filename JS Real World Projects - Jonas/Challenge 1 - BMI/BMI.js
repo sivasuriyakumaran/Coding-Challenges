4776d131bf2581d905ae0c1497b42be15266acea
sivasuriyakumaran <suriyakumaransivaprakash@gmail.com>
2019-03-27 14:47:11 +0530

    Completed Coding Challenge 1 - BMI Calulator from the course JS Real World Projects

---
+++ JS Real World Projects - Jonas/Challenge 1 - BMI/BMI.js
@@ -0,0 +1,8 @@
+var arjunWeight = 68,suriyaWeight = 62;
+var arjunHeight = 1.68,suriyaHeight = 1.68;
+var arjunBmi = arjunWeight/(arjunHeight*arjunHeight);
+var suriyaBmi= suriyaWeight/(suriyaHeight*suriyaHeight);
+var bmi = arjunBmi > suriyaBmi;
+console.log(arjunBmi);
+console.log(suriyaBmi);
+console.log(bmi);
